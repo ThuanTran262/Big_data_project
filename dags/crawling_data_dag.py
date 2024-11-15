51b029dd7a7d5f8383fb13b8df9c33b440356071
thuantt2 <thuantt2@vng.com.vn>
2024-11-15 11:12:40 +0700

    set up aiflow  dag

--- dags/crawling_data_dag.py
+++ dags/crawling_data_dag.py
@@ -1,19 +1,46 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
-from pipeline.
+from datetime import timedelta
 
-def helloWorld():
-    print('HelloWorld')
-    
+# import os
+# os.environ['PYTHONPATH'] = "/home/thuantt/airflow/Big_data_project"
+import sys
+sys.path.insert(0, "/home/thuantt/airflow/Big_data_project")
 
-with DAG(dag_id='hello_world_dag',
+from pipeline.crawling_data import insert_data_in_database_everyday 
+
+dag = DAG(dag_id='etl_data',
          start_date=datetime(2024,11,10),
-         schedule='@hourly',
-         catchup=False) as dag:
-    task1 = PythonOperator(
-        task_id='hello_word',
-        python_callable=helloWorld
-    )
+         schedule_interval='@daily',
+         catchup=False)
 
-task1
+# def helloWorld():
+#     print('Hello World')
+
+# dag = DAG(dag_id='hello_world_dag',
+#          start_date=datetime(2024,11,10),
+#          schedule_interval='@daily',
+#          catchup=False)
+
+start_task = DummyOperator(
+    task_id='start_task',
+    dag=dag)
+# task1 = PythonOperator(
+#         task_id='hello_world',
+#         python_callable=helloWorld,
+#         dag=dag)
+
+etl_data = PythonOperator(
+        task_id='etl_gold_data',
+        python_callable=insert_data_in_database_everyday,
+        dag=dag)
+
+end_task = DummyOperator(
+    task_id='end_task',
+    dag=dag,
+)
+
+# Set task dependencies
+start_task >> etl_data >> end_task
