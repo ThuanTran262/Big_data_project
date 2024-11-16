fd27c9e293c3f2497ce4df6e0d3b06a3c504f2d1
thuantt2 <thuantt2@vng.com.vn>
2024-11-16 23:43:38 +0700

    edit crawling data

--- utils/crawling_utils.py
+++ utils/crawling_utils.py
@@ -3,9 +3,9 @@ import datetime
 from dateutil.relativedelta import relativedelta
 from params import tables_params as tp
 
-START_DATE = (datetime.datetime.now() + relativedelta(hours=-48)).strftime("%Y-%m-%d")
+# START_DATE = (datetime.datetime.now() + relativedelta(hours=-48)).strftime("%Y-%m-%d")
+START_DATE = '2020-01-01'
 START_YEAR = (datetime.datetime.now()).strftime("%Y")
-END_DATE = (datetime.datetime.now() + relativedelta(hours=-24)).strftime("%Y-%m-%d")
 
 
 def crawl_data_every_minute():
@@ -20,14 +20,7 @@ def crawl_data_every_minute():
 
 
 def crawl_data_every_day():
-    current_date = datetime.datetime.now()
-    current_time = current_date.hour
-
-    if current_time >= 7:
-        current_date = (current_date + relativedelta(hours=-24)).strftime("%Y-%m-%d")
-
-    data = yf.download(tickers=tp.GOLD_TICKER, start=START_DATE, end=current_date)
-
+    data = yf.download(tickers=tp.GOLD_TICKER, start=START_DATE)
     data.reset_index(inplace=True)
     data.insert(0, tp.SYMBOL_COLUMN_ID, tp.GOLD_TICKER)
     data.columns = tp.COLUMNS_NAMES_LIST
@@ -35,9 +28,8 @@ def crawl_data_every_day():
     return data
 
 
-def crawl_data_from_beginning():
-    data = yf.download(tickers=tp.GOLD_TICKER)
-
+def crawl_data_in_current_year():
+    data = yf.download(tickers=tp.GOLD_TICKER, start=START_YEAR + "-01-01")
     data.reset_index(inplace=True)
     data.insert(0, tp.SYMBOL_COLUMN_ID, tp.GOLD_TICKER)
     data.columns = tp.COLUMNS_NAMES_LIST
